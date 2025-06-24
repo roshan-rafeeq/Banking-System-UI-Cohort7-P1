66676a67ecfaa50b209aedeb615bde5c15b8ad15
roshan-rafeeq <roshanrafeequevp07@gmail.com>
2025-06-24 11:25:04 +0530

    implement Loan Structure

--- src/routes/LoanRoutes.jsx
+++ src/routes/LoanRoutes.jsx
@@ -1,14 +1,15 @@
-// src/routes/LoanRoutes.jsx
-
 import { Routes, Route } from 'react-router-dom';
-// import ApplyLoan from '../modules/loan/pages/ApplyLoan';
-// import LoanList from '../modules/loan/pages/LoanList';
+import LoanTypes from '../modules/team4-loan-management/pages/LoanTypes';
+import GoldLoanCalculator from '../modules/team4-loan-management/pages/GoldLoanCalculator';
+import GoldLoanApply from '../modules/team4-loan-management/pages/GoldLoanApply';
 
 const LoanRoutes = () => {
   return (
     <Routes>
-      <Route path="/loan/apply" element={<ApplyLoan />} />
-      {/* <Route path="/loan/list" element={<LoanList />} /> */}
+      <Route path="/" element={<LoanTypes />} />
+      <Route path="gold" element={<GoldLoanCalculator />} />
+      <Route path="gold/apply" element={<GoldLoanApply />} />
+      {/* Add other loan types similarly */}
     </Routes>
   );
 };
